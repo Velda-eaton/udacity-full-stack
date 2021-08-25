ba5df17c59fb9fd35bae490a74d8f3154a9cbb25
Conaty, Velda <veldaconaty@eaton.com>
2021-08-25 15:44:30 +0100

    impove git ignore

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -2,10 +2,12 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from dotenv import load_dotenv
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+load_dotenv()
 
+database_name = os.environ.get('DB_NAME')
+database_path = os.environ.get('DB_PATH')
 db = SQLAlchemy()
 
 '''
--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -5,6 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -14,8 +18,9 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        
+        self.database_name = os.environ.get('DB_TEST_NAME')
+        self.database_path = os.environ.get('DB_PATH_TEST')
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
